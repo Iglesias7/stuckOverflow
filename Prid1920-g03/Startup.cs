c838b732607155a58aad746366985480dbc58f8a
Your NameIglesias <robaince.chendjou@yahoo.fr>
2019-10-21 13:01:04 +0200

    Ajout du front-end Angular au projet

--- Prid1920-g03/Startup.cs
+++ Prid1920-g03/Startup.cs
@@ -30,6 +30,12 @@ namespace Prid1920_g03
             services.AddDbContext<Prid1920_g03Context>(opt =>
                 opt.UseInMemoryDatabase("Prid1920-g03"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +54,29 @@ namespace Prid1920_g03
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseSpaStaticFiles();
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+                spa.Options.SourcePath = "ClientApp";
+                if (env.IsDevelopment())
+                {
+                    // Utilisez cette ligne si vous voulez que VS lance le front-end angular quand vous démarrez l'app
+                    //spa.UseAngularCliServer(npmScript: "start");
+                    // Utilisez cette ligne si le front-end angular est exécuté en dehors de VS (ou dans une autre instance de VS)
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
+            });
         }
     }
 }

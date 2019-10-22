a4d5d99b07c8d1a13867d8e94e4be68eaea25375
Merveil Nzitusu <m.genaud3a@live.be>
2019-10-22 17:13:33 +0200

    ajout deux routes login & restricted

--- Prid1920-g03/ClientApp/src/app/routing/app.routing.ts
+++ Prid1920-g03/ClientApp/src/app/routing/app.routing.ts
@@ -3,12 +3,28 @@ import { HomeComponent } from '../components/home/home.component';
 import { CounterComponent } from '../components/counter/counter.component';
 import { FetchDataComponent } from '../components/fetch-data/fetch-data.component';
 import { UserListComponent } from '../components/userlist/userlist.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RestrictedComponent } from '../components/restricted/restricted.component';
+import { UnknownComponent } from '../components/unknown/unknown.component';
+import { AuthGuard } from '../services/auth.guard';
+import { Role } from '../models/user';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'counter', component: CounterComponent },
   { path: 'fetch-data', component: FetchDataComponent },
-  { path: 'user', component: UserListComponent },
-  { path: '**', redirectTo: '' }
+  {
+    path: 'users',
+    component: UserListComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [Role.Admin] }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: 'restricted', component: RestrictedComponent },
+  { path: '**', component: UnknownComponent }
+
 ];
 
 export const AppRoutes = RouterModule.forRoot(appRoutes);

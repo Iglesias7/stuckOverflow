429b1a259f7b6894d13a6ad4daf43e930b8a7638
Merveil Nzitusu <m.genaud3a@live.be>
2019-11-12 17:56:28 +0100

    Début implémentations de validateurs asynchrones

--- Prid1920-g03/Controllers/UserController.cs
+++ Prid1920-g03/Controllers/UserController.cs
@@ -160,26 +160,41 @@ namespace Prid1920_g03.Controllers
         }
 
         [AllowAnonymous]
-        [HttpPost("validateuserbypseudoandemail")]
-        public async Task<ActionResult<User>> ValidateUserByPseudoAndEmail(UserDTO data){
-            var user = await ValidateUserByPseudoAndEmail(data.Pseudo, data.Email);
-            if(user.Pseudo == data.Pseudo)
-                return BadRequest(new ValidationErrors().Add("This pseudo is already used", "Pseudo"));
-            if(user.Email == data.Email)
-                return BadRequest(new ValidationErrors().Add("This email address is already used", "Email"));    
+        [HttpPost("validatepseudonottaken")]
+        public async Task<ActionResult<User>> ValidatePseudoNotTaken(UserDTO data){
+
+            var user = await ValidatePseudoNotTaken(data.Pseudo);
+            if(user!= null && user.Pseudo == data.Pseudo)
+                return BadRequest(new ValidationErrors().Add("This pseudo is already used", "Pseudo")); 
             return Ok(user);
         }
-        private async Task<User> ValidateUserByPseudoAndEmail(string pseudo,string email){
+        private async Task<User> ValidatePseudoNotTaken(string pseudo){
+           var user = await _context.Users.SingleOrDefaultAsync(u => u.Pseudo == pseudo);
 
-           var user = await _context.Users.SingleOrDefaultAsync( u => u.Pseudo == pseudo || u.Email == email);
-
-           //return null if user not found
            if(user == null)
                 return null;
             else
                 return user;
 
         }
+        [AllowAnonymous]
+        [HttpPost("validatemailnottaken")]
+        public async Task<ActionResult<User>> ValidateEmailNotTaken (UserDTO data){
+            var user = await ValidateEmailNotTaken(data.Email);
+            if(user!= null && user.Email == data.Email)
+                return BadRequest(new ValidationErrors().Add("This email address is already used", "Email")); 
+            return Ok(user);
+        }
+        private async Task<User> ValidateEmailNotTaken(string email){
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == email);
+
+           if(user == null)
+                return null;
+            else
+                return user;
+        }
+
+        
     }
 
 }

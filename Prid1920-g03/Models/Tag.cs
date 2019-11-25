541fd77407c0ffa34d711d8f108a84861ac1db79
Merveil Nzitusu <m.genaud3a@live.be>
2019-11-25 09:48:27 +0100

    classe Tag

--- Prid1920-g03/Models/Tag.cs
+++ Prid1920-g03/Models/Tag.cs
@@ -4,6 +4,10 @@ namespace Prid1920_g03.Models {
 
     public class Tag {
        public int Id {get; set;} 
-       public string Name {get; set;    }
+       public string Name {get; set;}
+
+       public Post Question {get; set;}
+
+       public Post Response {get; set;}
     }
 }

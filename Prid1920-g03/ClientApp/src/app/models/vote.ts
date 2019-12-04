663fcc3e795254205af05cb41fa76bd800b4e688
Your NameIglesias <robaince.chendjou@yahoo.fr>
2019-12-04 12:10:48 +0100

    conflit gerer

--- Prid1920-g03/ClientApp/src/app/models/vote.ts
+++ Prid1920-g03/ClientApp/src/app/models/vote.ts
@@ -1,12 +1,10 @@
 
 export class Vote {
-  id: number;
   updown: number; 
   authorId: number;
   postId: number;
 
   constructor(data: any) {
-    this.id = data.id;
     this.updown = 1 || -1;
     this.authorId = data.authorId;
     this.postId = data.postId;

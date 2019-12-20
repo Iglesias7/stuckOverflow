9219c2f84ee03e857b83b06037ea7e5e0e7d0e9d
LocalAstro <localastro@domepfc.net>
2019-12-20 18:39:34 +0100

    gerer la pagination avec les mats cards

--- Prid1920-g03/ClientApp/src/app/components/user/userlist/usersCard.component.ts
+++ Prid1920-g03/ClientApp/src/app/components/user/userlist/usersCard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource, MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatSnackBar, PageEvent, MatSortHeader } from '@angular/material';
 import * as _ from 'lodash';
 import { User } from '../../../models/user';
@@ -8,6 +8,8 @@ import { StateService } from 'src/app/services/state.service';
 import { MatTableState } from 'src/app/helpers/mattable.state';
 import { EditUserComponent } from '../edit-user/edit-user.component';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Observable } from 'rxjs';
+import { UserStateService } from 'src/app/services/userState.service';
 
 @Component({
     selector: 'app-userCard',
@@ -19,29 +21,32 @@ export class UserCardComponent implements AfterViewInit, OnDestroy {
     currentUser: User;
     users: User[] = [];
     usersBackup: User[] = [];
-
+    dataSource: MatTableDataSource<User> = new MatTableDataSource();
     filter: string;
+    state: MatTableState;
 
+    @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
 
     constructor(private userService: UserService,
-                private stateService: StateService,
+                private stateService: UserStateService,
                 private authServise: AuthenticationService,
                 public dialog: MatDialog,
                 public snackBar: MatSnackBar
         ) {
             this.currentUser = this.authServise.currentUser;
+            this.state = this.stateService.UserListState;
             }
 
     ngAfterViewInit(): void {
         // lie le datasource au sorter et au paginator
-
+        this.dataSource.paginator = this.paginator;
         // définit le predicat qui doit être utilisé pour filtrer les membres
         
-
         // établit les liens entre le data source et l'état de telle sorte que chaque fois que 
         // le tri ou la pagination est modifié l'état soit automatiquement mis à jour
 
         // récupère les données 
+        this.state.bind(this.dataSource);
         this.refresh();
     }
 
@@ -50,6 +55,8 @@ export class UserCardComponent implements AfterViewInit, OnDestroy {
             // assigne les données récupérées au datasource
             this.users = users;
             this.usersBackup = _.cloneDeep(users);
+            this.dataSource.data = this.users;
+            this.dataSource.paginator = this.paginator;
             // restaure l'état du filtre à partir du state
         });
     }
@@ -103,13 +110,23 @@ export class UserCardComponent implements AfterViewInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.snackBar.dismiss();
+        if(this.dataSource){
+            this.dataSource.disconnect();
+        }
     }
 
     filterChanged(filter: string) {
-        const lFilter = filter.toLowerCase();
-        this.users = _.filter(this.usersBackup, m => {
-            const str = (m.pseudo + ' ' + m.firstName).toLowerCase();
-            return str.includes(lFilter);
-        });
+        // const lFilter = filter.toLowerCase();
+        // this.users = _.filter(this.usersBackup, m => {
+        //     const str = (m.pseudo + ' ' + m.firstName).toLowerCase();
+        //     return str.includes(lFilter);
+
+        // });
+        this.dataSource.filter = filter.trim().toLowerCase();
+        this.state.filter = this.dataSource.filter;
+        if (this.dataSource.paginator)
+
+            this.dataSource.paginator.firstPage();
+
     }
 }
